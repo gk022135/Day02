cde794aeb3bf7574170cc2e726bc60a2d25a3c95
gk022135 <gk022135@gmail.com>
2025-08-07 01:20:45 +0530

    day 02 completye

---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def base(request):
+    return render(request,'home/base.html')
+
+def hello(request):
+    # return HttpResponse("<h1>hello this will working finse</h1>")
+    return render(request, 'home/base.html')
+
+def about(request):
+    data = ["gaurav", "saurav", "sachin"]
+    context = {"list_data": data}  # Key must be a string
+    return render(request, 'home/about.html', context)
+
+def home(request):
+    return render(request, 'home/home.html')
