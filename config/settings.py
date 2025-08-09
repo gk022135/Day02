22380dd25fcdce758ced8d3587989b4bf119e7e9
gk022135 <gk022135@gmail.com>
2025-08-09 21:43:30 +0530

    happy rakshabadhan

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+# import MySQLdb as Database
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # 👈 points to /templates folder
+        'DIRS': [BASE_DIR / 'templates'],  # points to /templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,9 +75,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+   
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'hello',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Gk022135@',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'gaurav_user',
+        'PASSWORD': 'gk022135@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class FirstTable(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()  # Removed max_length
+    phone = models.BigIntegerField()  # For large numbers like phone numbers
+    address = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
+
+
+class Cars(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    age = models.IntegerField()  # Removed max_length
+    phone = models.BigIntegerField()  # For large numbers like phone numbers
+    address = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
