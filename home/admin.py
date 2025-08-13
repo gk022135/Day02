d719ddfad4789927883a8ed9616f05c97f256418
gk022135 <gk022135@gmail.com>
2025-08-14 01:13:01 +0530

    day 04

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from home.models import *
 
-# Register your models here.
+# admin.site.register(FirstTable)
+
+admin.site.site_header = "Gaurav Admin"
+admin.site.site_title = "K"
+admin.site.site_url = "Expense Tracker"
+
+# customizing the admin interface
+class CustomAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "email",
+        "age",
+        "phone"
+    ]
+
+# add the search field, list filter, ordering 
+    search_fields = ['name', 'email']  # can take multiple value for make seacrh regarding that
+    list_filter = ['age']  # take multiple field make fillter regarding that
+    ordering = ['-phone']  #
+
+
+admin.site.register(FirstTable, CustomAdmin)
+
+# Implementing Search_fields, filters and ordering in the admin Panel
