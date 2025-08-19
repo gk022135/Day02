10c09893981890c872c4d2d72b6e33108bd74c3f
gk022135 <gk022135@gmail.com>
2025-08-19 14:36:38 +0530

    hello from lap

--- config/middleware/RequestLog.py
+++ config/middleware/RequestLog.py
@@ -1,7 +1,22 @@
-class RequestLog:
-    def __init__(self, get_response) -> None:
+from home.models import RequestLog
+
+class RequestLogMiddleware:
+    def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        request_info = request
-        RequestLog
+        # Process request before view
+        response = self.get_response(request)
+
+        # Save log to DB after response is generated
+        try:
+            RequestLog.objects.create(
+                request_log = request.path,        # the URL path
+                request_method = request.method,   # GET/POST/etc.
+                request_type = request.META.get("CONTENT_TYPE", ""), # request type (e.g. JSON, form-data)
+            )
+        except Exception as e:
+            # Prevent logging errors from breaking the app
+            print("Logging failed:", e)
+
+        return response
--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.middleware.RequestLog.RequestLogMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
